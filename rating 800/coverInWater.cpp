8198e9ebe9cd72ac626904ec71519e480169d4ba
Dheerajverma <dheerajku357@gmail.com>
2026-01-10 15:08:16 +0530

    Cover in water problem added

---
+++ rating 800/coverInWater.cpp
@@ -0,0 +1,32 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int coverInWater(){
+     string s;
+     int n;
+     cin>>n;
+     cin>>s;
+     int ans=0;
+     int maxdot=0;
+     int dot=0;
+     for(int i=0;i<s.size();i++){
+          if(s[i]=='.') {
+                dot++;
+                ans++;
+          }
+          maxdot=max(dot,maxdot);
+          if(s[i]=='#') dot=0;
+          
+          
+     }
+     if(maxdot>2) return 2;
+
+     return ans;
+}
+int main(){
+     int t;
+     cin>>t;
+     while(t--){
+          cout<<coverInWater()<<endl;
+     }
+}
